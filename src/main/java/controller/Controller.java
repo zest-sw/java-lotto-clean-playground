b423ec62cc7ef57a265aaf5fa62bd8e00223591f
TheDeveloperKwon <akongwon@gmail.com>
2026-03-31 21:15:34 +0900

    feat: controller 클래스 추가

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,33 @@
+package controller;
+
+import domain.LottoService;
+import domain.LottoTicket;
+import domain.Statistic;
+import util.RandomNumberGenerator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
+public class Controller {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    LottoService lottoService = new LottoService(new RandomNumberGenerator());
+    Statistic statistic = new Statistic();
+
+
+    public void run(){
+        int money = inputView.getMoney();
+
+        List<LottoTicket> lottoTickets = lottoService.buyTickets(money);
+
+        outputView.printLottoList(lottoTickets.size(), lottoTickets);
+
+        List<Integer> winningNumbers = inputView.getWinningNumbers();
+
+        List<Integer> winningResult = statistic.getWinningResult(lottoTickets, winningNumbers);
+        double revenue = statistic.getRevenue(money, winningResult);
+
+        outputView.printResult(winningResult, revenue);
+    }
+}
