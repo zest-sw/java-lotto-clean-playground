97e847a452a94ee9aa6a26c36ebffe25db85119e
TheDeveloperKwon <akongwon@gmail.com>
2026-04-06 17:21:53 +0900

    refactor: winningResult 구조 개선

--- src/main/java/domain/Statistic.java
+++ src/main/java/domain/Statistic.java
@@ -1,11 +1,10 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Statistic {
 
-    public static final int WINNING_COUNT = 1;
+
     public static final int MATCHED_THREE_PRIZE = 5000;
     public static final int MATCHED_FOUR_PRIZE = 50000;
     public static final int MATCHED_FIVE_PRIZE = 1500000;
@@ -15,20 +14,22 @@ public class Statistic {
     public static final int MATCHED_FIVE_COUNT = 5;
     public static final int MATCHED_SIX_COUNT = 6;
 
-    public List<Integer> getWinningResult(List<LottoTicket> lottoTickets, List<Integer> winningNumbers) {
-        List<Integer> winningResult = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0));
+    public WinningResult getWinningResult(List<LottoTicket> lottoTickets, List<Integer> winningNumbers) {
+        WinningResult winningResult = new WinningResult();
+
         for(LottoTicket lottoTicket: lottoTickets) {
-            int index = lottoTicket.getMatchedNumbers(winningNumbers);
-            winningResult.set(index, winningResult.get(index) + WINNING_COUNT);
+            int matchedCount = lottoTicket.getMatchedNumbers(winningNumbers);
+
+            winningResult.add(matchedCount);
         }
         return winningResult;
     }
 
-    public double getRevenue(int money, List<Integer> winningResult){
-        double prize = MATCHED_THREE_PRIZE * winningResult.get(MATCHED_THREE_COUNT)
-                     + MATCHED_FOUR_PRIZE  * winningResult.get(MATCHED_FOUR_COUNT)
-                     + MATCHED_FIVE_PRIZE  * winningResult.get(MATCHED_FIVE_COUNT)
-                     + MATCHED_SIX_PRIZE   * winningResult.get(MATCHED_SIX_COUNT);
+    public double getRevenue(int money, WinningResult winningResult){
+        double prize = MATCHED_THREE_PRIZE * winningResult.getCountOf(MATCHED_THREE_COUNT)
+                     + MATCHED_FOUR_PRIZE  * winningResult.getCountOf(MATCHED_FOUR_COUNT)
+                     + MATCHED_FIVE_PRIZE  * winningResult.getCountOf(MATCHED_FIVE_COUNT)
+                     + MATCHED_SIX_PRIZE   * winningResult.getCountOf(MATCHED_SIX_COUNT);
         return prize/money;
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import domain.LottoTicket;
+import domain.WinningResult;
 
 import java.util.List;
 
@@ -14,13 +15,13 @@ public class OutputView {
         }
     }
 
-    public void printResult(List<Integer> winningResult, double revenue) {
+    public void printResult(WinningResult winningResult, double revenue) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.println("3개 일치 (5000원)- "+ winningResult.get(3) +"개");
-        System.out.println("4개 일치 (50000원)- "+ winningResult.get(4) +"개");
-        System.out.println("5개 일치 (1500000원)- "+ winningResult.get(5) +"개");
-        System.out.println("6개 일치 (2000000000원)- "+ winningResult.get(6) +"개");
+        System.out.println("3개 일치 (5000원)- "+ winningResult.getCountOf(3) +"개");
+        System.out.println("4개 일치 (50000원)- "+ winningResult.getCountOf(4) +"개");
+        System.out.println("5개 일치 (1500000원)- "+ winningResult.getCountOf(5) +"개");
+        System.out.println("6개 일치 (2000000000원)- "+ winningResult.getCountOf(6) +"개");
         System.out.println("총 수익률은 "+ revenue +"입니다.");
 
     }
