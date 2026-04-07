7eac886c1e55f4570a2c542517c39ed1a1f5a713
seungwon <96052481+zest-sw@users.noreply.github.com>
2026-04-07 19:35:53 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/domain/LottoService.java
@@ -0,0 +1,48 @@
+package domain;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoService {
+    public static final int TICKET_PRICE = 1000;
+    public static final int LOTTO_SIZE = 6;
+
+    public LottoService() {
+    }
+
+    public List<LottoTicket> buyTickets(int money) {
+        validateMoney(money);
+        int ticketCount = money/ TICKET_PRICE;
+
+        List<LottoTicket> tickets = new ArrayList<>();
+
+        for(int i=0;i<ticketCount;i++){
+            tickets.add(generateTicket());
+        }
+
+        return tickets;
+    }
+
+    private LottoTicket generateTicket() {
+        List<Integer> ticketBasket = new ArrayList<>();
+        List<Integer> ticketNumbers = new ArrayList<>();
+
+        ticketBasket = IntStream.rangeClosed(1,45)
+                                .boxed()
+                                .collect(Collectors.toList());
+        Collections.shuffle(ticketBasket);
+        ticketNumbers = ticketBasket.subList(0, 6);
+
+        return new LottoTicket(ticketNumbers);
+    }
+
+    private void validateMoney(int money) {
+        if(money < TICKET_PRICE) {
+            throw new IllegalArgumentException("1000원 이상의 금액을 입력해주세요.");
+        }
+    }
+}
